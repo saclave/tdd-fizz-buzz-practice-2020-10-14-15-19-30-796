e501b5b4329211d194852cf6a062332d7d9b0847
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-15 14:02:46 +0800

    [FizzBuzz][Vea] test - test when number is multiple of 3

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,26 @@
 package tdd.fizzbuzz;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class FizzBuzzTest {
+    private static final String FIZZ = "FIZZ";
+    private int num;
+
+    @BeforeEach
+    public void setUp() { }
+
+    @Test
+    public void test_when_number_is_a_multiple_of_3() {
+        num = 3;
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals(FIZZ, fizzBuzz.getOrderNumber(num));
+    }
+
+    @Test
 }
+
+//multiple of 3 Fizz
