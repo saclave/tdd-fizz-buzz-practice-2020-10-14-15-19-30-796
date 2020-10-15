b0551f5574ee900aaa734f3db816061baa3c8d40
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-15 14:10:31 +0800

    [FIZZBUZZ][VEA] FEATURE - MODULO 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,15 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
 
     public String sortOrderNumber(int orderNumber) {
         if(orderNumber % 3 == 0){
             return FIZZ;
+        }
+
+        if(orderNumber % 3 == 0){
+            return BUZZ;
         }
         return null;
     }
